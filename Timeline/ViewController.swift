4783304d9171b6aaf8701d64b8175d6b2c878af2
Emma Suttell <emsuttell@gmail.com>
2020-08-06 11:54:41 -0700

    Initial Commit

---
+++ Timeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timeline
+//
+//  Created by Apple on 8/6/20.
+//  Copyright © 2020 Emma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
